2abb228801b130428d516d1895686d1a503907a0
oschebell <oschebell@gmail.com>
2016-02-08 23:25:21 +1100

    fixed syntax error

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
-  validates :content, length: { maximum: 140 }
-                      prescence: true
+  validates :content, length: { maximum: 140 },
+                      presence: true
 end
